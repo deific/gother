dc1f73834eab3c48e056bf24314a6a2c632589e5
deific <deific@126.com>
2022-05-02 22:46:16 +0800

    fix utxo test

--- chapter7/test.sh
+++ chapter7/test.sh
@@ -28,12 +28,14 @@ mkdir ./tmp/test/utxo
 ./gother balance -refname One    # Steven: 800 Two:130 One:70
 ./gother balance -refname Two    # Steven: 800 Two:130 One:70
 ./gother sendbyrefname -from Two -to Steven -amount 90
-./gother sendbyrefname -from Two -to One -amount 90
 ./gother mine
 ./gother blockchaininfo
 ./gother balance -refname Steven # Steven: 890 Two:40 One:70
 ./gother balance -refname One # Steven: 890 Two:40 One:70
 ./gother balance -refname Two # Steven: 890 Two:40 One:70
+
+./gother sendbyrefname -from Two -to One -amount 90
+./gother mine
 
 ./gother balance2 -refname Steven # Steven: 890 Two:40 One:70
 ./gother balance2 -refname One # Steven: 890 Two:40 One:70
